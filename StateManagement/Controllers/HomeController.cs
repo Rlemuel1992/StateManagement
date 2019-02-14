4bfe427159d86d0bab8ecb9108281884fbd54e80
Ronald Lemuel <Rlemuel1992@gmail.com>
2019-02-14 16:52:43 -0500

    First commit

---
+++ StateManagement/Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using StateManagement.Models;
+
+namespace StateManagement.Controllers
+{
+	public class HomeController : Controller
+	{
+		public List<User> Logins = new List<User>();
+
+		public ActionResult Index()
+		{
+			ViewBag.CurrentUser = Session["CurrentUser"];
+			return View();
+		}
+
+
+
+		public ActionResult Contact()
+		{
+			ViewBag.CurrentUser = Session["CurrentUser"];
+			return View();
+		}
+		public ActionResult RegisterUser()
+		{
+			return View();
+		}
+		public ActionResult UserDetails(User u)
+		{
+			if (Session["CurrentUser"] != null)
+			{
+				Logins.Add(u);
+				u = (User)Session["CurrentUser"];
+				Session["CurrentUser"] = u;
+				ViewBag.CurrentUser = u;
+				
+				return View();
+			}
+			else
+			{
+				Logins.Add(u);
+				ViewBag.CurrentUser = u;
+				Session["CurrentUser"] = u;
+
+				return RedirectToAction("Index");
+				
+			}
+		}
+
+		public ActionResult LogOut()
+		{
+			Session.Remove("CurrentUser");
+			return RedirectToAction("Index");
+		}
+		public ActionResult Login()
+		{
+			
+			return View();
+		}
+	}
+}
---
+++ StateManagement/Models/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StateManagement.Models
+{
+	public class User
+	{
+		public string UserName { get; set; }
+		public string Password { get; set; }
+		public string Email {get; set;}
+		public int Age { get; set; }
+
+		public User(string UserName, string Email, string Password, int Age)
+		{
+			this.UserName = UserName;
+			this.Email = Email;
+			this.Password = Password;
+			this.Age = Age;
+		}
+
+		public User() { }
+	}
+}
